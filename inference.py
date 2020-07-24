a86779f0d67272d939522cb15c693ed73b990210
SSchwoebel <sarah.schwoebel@gmail.com>
2020-07-24 14:49:43 +0200

    modified group inference

--- inference.py
+++ inference.py
@@ -10,7 +10,7 @@ import pymc3 as pm
 import numpy as np
 import matplotlib.pyplot as plt
 
-import world 
+import world
 import agent as agt
 import perception as prc
 
@@ -31,7 +31,7 @@ class LogLike(tt.Op):
     that define our model) and returning a single "scalar" value (the
     log-likelihood)
     """
-    itypes = [tt.lvector] # expects a vector of parameter values when called
+    itypes = [tt.dvector] # expects a vector of parameter values when called
     otypes = [tt.dmatrix, tt.dmatrix] # outputs a single scalar value (the log likelihood)
 
     def __init__(self, loglike, fixed):
@@ -49,25 +49,25 @@ class LogLike(tt.Op):
 
         outputs[0][0] = np.array(probs_a) # output the log-likelihood
         outputs[1][0] = np.array(probs_r) # output the log-likelihood
-        
-        
+
+
 class Inferrer:
     def __init__(self, worlds):
-        
+
         self.nruns = len(worlds)
-        
+
         w = worlds[0]
-        
+
         self.setup_agent(w)
-        
+
         self.actions = np.array([w.actions[:,0] for w in worlds])
-        
+
         self.rewards = np.array([w.rewards[:,1] for w in worlds])
-        
+
         self.inferrer = LogLike(self.agent.fit_model, self.fixed)
-    
+
     def setup_agent(self, w):
-        
+
         ns = w.environment.Theta.shape[0]
         nr = w.environment.Rho.shape[1]
         na = w.environment.Theta.shape[2]
@@ -80,128 +80,161 @@ class Inferrer:
         utility = w.agent.perception.prior_rewards.copy()
         A = w.agent.perception.generative_model_observations.copy()
         B = w.agent.perception.generative_model_states.copy()
-        
+
         transition_matrix_context = w.agent.perception.transition_matrix_context.copy()
-        
-        # concentration parameters    
+
+        # concentration parameters
         C_alphas = np.ones((nr, ns, nc))
         # initialize state in front of levers so that agent knows it yields no reward
         C_alphas[0,0,:] = 100
         for i in range(1,nr):
             C_alphas[i,0,:] = 1
-        
+
         # agent's initial estimate of reward generation probability
         C_agent = np.zeros((nr, ns, nc))
         for c in range(nc):
             C_agent[:,:,c] = np.array([(C_alphas[:,i,c])/(C_alphas[:,i,c]).sum() for i in range(ns)]).T
-        
+
         pol = w.agent.policies.copy()
-        
+
         #pol = pol[-2:]
         npi = pol.shape[0]
-        
+
         # prior over policies
-        
+
         alpha = 1
         alphas = np.zeros_like(w.agent.perception.dirichlet_pol_params.copy()) + alpha
-        
+
         prior_pi = alphas.copy()
         prior_pi /= prior_pi.sum(axis=0)
-        
+
         state_prior = np.zeros((ns))
-        
+
         state_prior[0] = 1.
-        
+
         prior_context = np.zeros((nc)) + 1./(nc)#np.dot(transition_matrix_context, w.agent.posterior_context[-1,-1])
-            
+
     #    prior_context[0] = 1.
-            
+
         pol_par = alphas
-    
+
         # perception
         bayes_prc = prc.HierarchicalPerception(A, B, C_agent, transition_matrix_context, state_prior, utility, prior_pi, pol_par, C_alphas, T=T)
-        
+
         bayes_pln = agt.BayesianPlanner(bayes_prc, None, pol,
                           trials = trials, T = T,
                           prior_states = state_prior,
                           prior_policies = prior_pi,
-                          number_of_states = ns, 
+                          number_of_states = ns,
                           prior_context = prior_context,
                           learn_habit = True,
                           #save_everything = True,
                           number_of_policies = npi,
                           number_of_rewards = nr)
-        
+
         self.agent = world.FakeWorld(bayes_pln, observations, rewards, actions, trials = trials, T = T)
-        
+
         self.fixed = {'rew_mod': C_agent, 'beta_rew': C_alphas}
-        
+
     def run_single_inference(self, idx, ndraws=300, nburn=100, cores=4):
-        
-        self.idx = idx
-        
-        curr_model = self.single_model(idx)
-        
-        with curr_model:
-            
-            step = pm.Metropolis()#S=np.ones(1)*0.01)
-        
-            trace = pm.sample(ndraws, tune=nburn, discard_tuned_samples=True, step=step, cores=cores)
-        
-            # plot the traces
-#            plt.figure()
-#            _ = pm.traceplot(trace)#, lines=('h', 1./alpha_true))
-#            plt.show()
-#            plt.figure()
-#            _ = pm.plot_posterior(trace, var_names=['h'], ref_val=(1./alpha_true))
-#            plt.show()
-            
-            # save the traces
-            fname = pm.save_trace(trace)
-            
-        return fname
-        
-    def run_group_inference(self, ndraws=300, nburn=100, cores=5):
-        
-        curr_model = self.group_model()
-        
-        with curr_model:
-            
-            step = pm.Metropolis()#S=np.ones(1)*0.01)
-        
-            trace = pm.sample(ndraws, tune=nburn, discard_tuned_samples=True, step=step, cores=cores)
-        
-            # plot the traces
-#            plt.figure()
-#            _ = pm.traceplot(trace)#, lines=('h', 1./alpha_true))
-#            plt.show()
-#            plt.figure()
-#            _ = pm.plot_posterior(trace, var_names=['h'], ref_val=(1./alpha_true))
-#            plt.show()
-            
-            # save the traces
-            fname = pm.save_trace(trace)
-            
-        return fname
-    
-    def single_model(self, idx):
-        
+
+        minimum = 0.
+        maximum = 8.
+        sample_space = np.arange(minimum, maximum+1, 1)
+        sample_space = 1./10**(sample_space/4.)
+
         with pm.Model() as smodel:
             # uniform priors on h
-            hab_ten = pm.DiscreteUniform('h', 0., 20.)
-            
+            hab_ten = pm.DiscreteUniform('h', minimum, maximum)
+
             # convert to a tensor
-            alpha = tt.as_tensor_variable([hab_ten*5+1])
+            alpha = tt.as_tensor_variable([10**(hab_ten/4.)])
             probs_a, probs_r = self.inferrer(alpha)
-        
+
             # use a DensityDist
             pm.Categorical('actions', probs_a, observed=self.actions[idx])
             pm.Categorical('rewards', probs_r, observed=self.rewards[idx])
-            
-        return smodel
-    
+
+            step = pm.Metropolis()#S=np.ones(1)*0.01)
+
+            trace = pm.sample(ndraws, tune=nburn, discard_tuned_samples=True, step=step, cores=cores)
+
+            # plot the traces
+#            plt.figure()
+#            _ = pm.traceplot(trace)#, lines=('h', 1./alpha_true))
+#            plt.show()
+#            plt.figure()
+#            _ = pm.plot_posterior(trace, var_names=['h'], ref_val=(1./alpha_true))
+#            plt.show()
+
+            # save the traces
+            #fname = pm.save_trace(trace)
+            samples = trace['h', nburn:]
+
+        return samples, sample_space
+
+    def analyze_samples(self, samples, sample_space):
+
+        dist = np.array([(samples == i).sum() for i in range(len(sample_space))])
+
+        return dist
+
+    def analyze_dist(self, dist, sample_space):
+
+        mean = (dist * sample_space).sum()
+
+        variance = (dist * np.abs((sample_space - mean)**2)).sum()
+
+        mode = sample_space[np.argmax(dist)]
+
+        return mode, mean, variance
+
+    def run_group_inference(self, ndraws=300, nburn=100, cores=5):
+
+        curr_model = self.group_model()
+
+        with curr_model:
+
+            step = pm.Metropolis()#S=np.ones(1)*0.01)
+
+            trace = pm.sample(ndraws, tune=nburn, discard_tuned_samples=True, step=step, cores=cores)
+
+            # plot the traces
+#            plt.figure()
+#            _ = pm.traceplot(trace)#, lines=('h', 1./alpha_true))
+#            plt.show()
+#            plt.figure()
+#            _ = pm.plot_posterior(trace, var_names=['h'], ref_val=(1./alpha_true))
+#            plt.show()
+
+            # save the traces
+            fname = pm.save_trace(trace)
+
+        return fname
+
+    def single_model(self, idx):
+
+        minimum = 0.
+        maximum = 8.
+        sample_space = np.arange(minimum, maximum, 1)
+        sample_space = 1./10**(sample_space/4.)
+
+        with pm.Model() as smodel:
+            # uniform priors on h
+            hab_ten = pm.DiscreteUniform('h', 0., 8.)
+
+            # convert to a tensor
+            alpha = tt.as_tensor_variable([10**(hab_ten/4.)])
+            probs_a, probs_r = self.inferrer(alpha)
+
+            # use a DensityDist
+            pm.Categorical('actions', probs_a, observed=self.actions[idx])
+            pm.Categorical('rewards', probs_r, observed=self.rewards[idx])
+
+        return smodel, sample_space
+
     def group_model(self):
-        
+
         with pm.Model() as gmodel:
             # uniform priors on h
             m = pm.DiscreteUniform('h', 0., 20.)
@@ -209,43 +242,43 @@ class Inferrer:
             mean = 2*m+1
             alphas = np.arange(1., 101., 5.)
             p = self.discreteNormal(alphas, mean, std)
-            
+
             for i in range(self.nruns):
                 hab_ten = pm.Categorical('h_{}'.format(i), p)
-                
+
                 alpha = tt.as_tensor_variable([hab_ten])
                 probs_a, probs_r = self.inferrer(alpha)
-            
+
                 # use a DensityDist
                 pm.Categorical('actions_{}'.format(i), probs_a, observed=self.actions[i])
                 pm.Categorical('rewards_{}'.format(i), probs_r, observed=self.rewards[i])
-                
+
         return gmodel
 
     def discreteNormal(self, x, mean, std):
-        
+
         p = np.exp(-(x - mean)**2/(2*std**2))
         p /= p.sum()
         return p
-    
-    def plot_inference(self, trace_name, model='single', idx=None):
-        
-        if model=='single':
-            curr_model = self.single_model(idx)
-        elif model=='group':
-            curr_model = self.group_model()
-        
-        with curr_model:
-            
-            # save the traces
-            trace = pm.load_trace(trace_name)
-        
-            # plot the traces
-            plt.figure()
-            _ = pm.traceplot(trace)#, lines=('h', 1./alpha_true))
-            plt.show()
+
+    def plot_inference(self, samples, model='single', idx=None):
+
+        pass
+        # if model=='single':
+        #     curr_model = self.single_model(idx)
+        # elif model=='group':
+        #     curr_model = self.group_model()
+
+        # with curr_model:
+
+        #     # save the traces
+        #     trace = pm.load_trace(trace_name)
+
+        #     # plot the traces
+        #     plt.figure()
+        #     _ = pm.traceplot(trace)#, lines=('h', 1./alpha_true))
+        #     plt.show()
     #        plt.figure()
     #        _ = pm.plot_posterior(trace, var_names=['h'], ref_val=(1./alpha_true))
     #        plt.show()
-        
-        
+
--- parameter_recovery.py
+++ parameter_recovery.py
@@ -8,7 +8,7 @@ Created on Wed Jul  8 17:44:57 2020
 
 import numpy as np
 
-import world 
+import world
 import agent as agt
 import perception as prc
 import inference as infer
@@ -16,7 +16,7 @@ import matplotlib.pylab as plt
 import jsonpickle as pickle
 import jsonpickle.ext.numpy as jsonpickle_numpy
 import json
-#import seaborn as sns
+import seaborn as sns
 import pandas as pd
 import os
 np.set_printoptions(threshold = 100000, precision = 5)
@@ -25,83 +25,97 @@ import pymc3 as pm
 import theano
 import theano.tensor as tt
 import gc
-    
+
 
 
 def run_fitting(folder):
-    
-    for tendency in [100]:
-        for trans in [99]:
-            print(tendency, trans)
-            traces = []
-                
-            run_name ="h"+str(tendency)+"_t"+str(trans)+"_p90_train100.json"
-            fname = os.path.join(folder, run_name)
-            
-            jsonpickle_numpy.register_handlers()
-            
-            with open(fname, 'r') as infile:
-                data = json.load(infile)
-                
-            worlds_old = pickle.decode(data)
-            
-            inferrer = infer.Inferrer(worlds_old)
-            
 
-            traces = inferrer.run_single_inference(5, ndraws=3000, nburn=1000, cores=4)
-            
-            #traces = inferrer.run_group_inference(ndraws=300, nburn=100, cores=4)
-                
-            fname = os.path.join(folder, run_name[:-5]+"_traces.json")
-                    
-            jsonpickle_numpy.register_handlers()
-            pickled = pickle.encode(traces)
-            with open(fname, 'w') as outfile:
-                json.dump(pickled, outfile)
-            
-            pickled = 0
-            traces = 0
-            
-            gc.collect()
-            
-            
-def load_fitting(folder):
-    
-    for tendency in [100]:
+    for tendency in [1]:
         for trans in [99]:
             print(tendency, trans)
             traces = []
-                
+
             run_name ="h"+str(tendency)+"_t"+str(trans)+"_p90_train100.json"
             fname = os.path.join(folder, run_name)
-            
+
             jsonpickle_numpy.register_handlers()
-            
+
             with open(fname, 'r') as infile:
                 data = json.load(infile)
-                
+
             worlds_old = pickle.decode(data)
-            
-            repetitions = len(worlds_old)
-            
+
             inferrer = infer.Inferrer(worlds_old)
-                
+
+            likelihood = 1
+
+            for i in range(4,len(worlds_old)):
+                print("agent ", i)
+                samples, sample_space = inferrer.run_single_inference(i, ndraws=3000, nburn=1000, cores=4)
+                dist = inferrer.analyze_samples(samples, sample_space)
+                likelihood *= dist
+
+                fname = os.path.join(folder, run_name[:-5]+"_samples_"+str(i)+".json")
+
+                jsonpickle_numpy.register_handlers()
+                pickled = pickle.encode(traces)
+                with open(fname, 'w') as outfile:
+                    json.dump(pickled, outfile)
+
+                pickled = 0
+
+            likelihood /= likelihood.sum()
+            mode, mean, variance = inferrer.analyze_dist(likelihood, sample_space)
+            print(mode, mean, variance)
+
+            #traces = inferrer.run_group_inference(ndraws=300, nburn=100, cores=4)
+
+            gc.collect()
+
+            plt.figure()
+            sns.distplot(dist)
+            plt.show()
+
+            return samples, sample_space
+
+
+def load_fitting(folder):
+
+    for tendency in [1]:
+        for trans in [99]:
+            print(tendency, trans)
+            traces = []
+
             run_name ="h"+str(tendency)+"_t"+str(trans)+"_p90_train100.json"
-            
-            fname = os.path.join(folder, run_name[:-5]+"_traces.json")
-            
+            fname = os.path.join(folder, run_name)
+
+            jsonpickle_numpy.register_handlers()
+
             with open(fname, 'r') as infile:
                 data = json.load(infile)
-                
+
+            worlds_old = pickle.decode(data)
+
+            repetitions = len(worlds_old)
+
+            inferrer = infer.Inferrer(worlds_old)
+
+            run_name ="h"+str(tendency)+"_t"+str(trans)+"_p90_train100.json"
+
+            fname = os.path.join(folder, run_name[:-5]+"_samples.json")
+
+            with open(fname, 'r') as infile:
+                data = json.load(infile)
+
             trace_name = pickle.decode(data)
-            
+
             inferrer.plot_inference(trace_name, model='single', idx=5)
-            
+
             pickled = 0
             traces = 0
-            
+
             gc.collect()
-                        
+
 
 
 def main():
@@ -109,7 +123,7 @@ def main():
     """
     set parameters
     """
-    
+
     T = 2 #number of time steps in each trial
     nb = 2
     no = nb+1 #number of observations
@@ -119,35 +133,35 @@ def main():
     nr = nb+1
     nc = nb #1
     n_parallel = 1
-    
+
     folder = "data"
     if not os.path.isdir(folder):
         raise Exception("run_rew_prob_simulations() needs to be run first")
-        
+
     run_args = [T, ns, na, nr, nc]
-    
+
     u = 0.99
     utility = np.zeros(nr)
     for i in range(1,nr):
         utility[i] = u/(nr-1)
     utility[0] = (1.-u)
-    
+
     repetitions = 20
-    
+
     avg = True
-    
+
     #run_fitting(folder)
-    
+
     load_fitting(folder)
-    
-    
+
+
 if __name__ == "__main__":
-    
+
 
     """
     set parameters
     """
-    
+
     T = 2 #number of time steps in each trial
     nb = 2
     no = nb+1 #number of observations
@@ -157,24 +171,24 @@ if __name__ == "__main__":
     nr = nb+1
     nc = nb #1
     n_parallel = 1
-    
+
     folder = "data"
     if not os.path.isdir(folder):
         raise Exception("run_rew_prob_simulations() needs to be run first")
-        
+
     run_args = [T, ns, na, nr, nc]
-    
+
     u = 0.99
     utility = np.zeros(nr)
     for i in range(1,nr):
         utility[i] = u/(nr-1)
     utility[0] = (1.-u)
-    
+
     repetitions = 20
-    
+
     avg = True
-    
-    run_fitting(folder)
-    
-    load_fitting(folder)
-    
+
+    samples = run_fitting(folder)
+
+    #load_fitting(folder)
+
