1ae7df127e9af4f02e6b956cae84f3c04beeb002
SSchwoebel <sarah.schwoebel@gmail.com>
2022-06-21 17:28:51 +0200

    working on nicer plots for RT sampling trajectories

--- run_trajectory_examples.py
+++ run_trajectory_examples.py
@@ -13,9 +13,12 @@ import action_selection as asl
 import seaborn as sns
 import pandas as pd
 from misc import *
+import os
 #sns.set_style("whitegrid", {"axes.edgecolor": "0.15"})#, "axes.spines.top": "False", "axes.spines.right": "False"})
 #plt.style.use('seaborn-darkgrid')
 plt.style.use('seaborn-whitegrid')
+
+folder = '/home/sarah/Nextcloud/RT_paper/RT_sampling_figs/'
 
 # test function
 def run_action_selection(post, prior, like, trials = 100, crit_factor = 0.5):
@@ -30,8 +33,8 @@ def run_action_selection(post, prior, like, trials = 100, crit_factor = 0.5):
     return ac_sel.RT.squeeze().astype(int), samples
 
 
-def show_sampling_trajectory(samples, RT, npi, j=0):
-
+def show_sampling_trajectory(samples, RT, npi, j=0, crit_factor = 0.5):
+    print(RT)
     Dir_counts = np.zeros((RT+1,npi)) + 1
     estimated_q = np.zeros((RT+1,npi)) + 1./npi
     for i,s in enumerate(samples):
@@ -41,29 +44,50 @@ def show_sampling_trajectory(samples, RT, npi, j=0):
         estimated_q[i+1] = Dir_counts[i+1] / Dir_counts[i+1].sum()
 
     plt.figure()
+    plt.plot([post[0]]*(6000), '--', color='gray', label='true posterior', linewidth=3)
     plt.plot(estimated_q[:,0], label='sampled posterior', linewidth=3)
-    plt.plot([post[0]]*(RT+1), '--', color='gray', label='true posterior', linewidth=3)
     #plt.plot([post[1]]*RT, '--', color='gray')
-    plt.xlim([0,RT+1])
+    plt.xlim([0,6000])
     plt.ylim([0,1])
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
     plt.ylabel('q(a)',fontsize=16)
     plt.xlabel('sample number',fontsize=16)
-    plt.legend()
-    plt.savefig('sampling_run_'+str(npi)+'_'+str(j)+'.svg')
-    plt.savefig('sampling_run_'+str(npi)+'_'+str(j)+'.png')
+    plt.legend(fontsize=14, loc=4)
+    plt.savefig(os.path.join(folder,'sampling_run_npi'+str(npi)+'_f'+str(f)+'_'+str(j)+'.svg'))
+    plt.savefig(os.path.join(folder,'sampling_run_npi'+str(npi)+'_f'+str(f)+'_'+str(j)+'.png'))
+    plt.show()
+    
+    plt.figure()
+    plt.plot([post[0]]*(6000), '--', color='gray', label='true posterior action 1', linewidth=3)
+    plt.plot(estimated_q[:,0], label='sampled posterior action 1', linewidth=3)
+    plt.plot(estimated_q[:,1:], linewidth=3)#, label='sampled posterior other actions'
+    #plt.plot([post[1]]*RT, '--', color='gray')
+    plt.xlim([0,6000])
+    plt.ylim([0,1])
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.ylabel('q(a)',fontsize=16)
+    plt.xlabel('sample number',fontsize=16)
+    plt.legend(fontsize=14, loc=4)
+    plt.savefig(os.path.join(folder,'sampling_run_all_npi'+str(npi)+'_f'+str(f)+'_'+str(j)+'.svg'))
+    plt.savefig(os.path.join(folder,'sampling_run_all_npi'+str(npi)+'_f'+str(f)+'_'+str(j)+'.png'))
     plt.show()
 
     plt.figure()
-    logit = ln(estimated_q[:,0]/(1-estimated_q[:,0]))
     logit_true = ln(post[0] / (1-post[0]))
+    logit = ln(estimated_q[:,0]/(1-estimated_q[:,0]))
     plt.plot(logit, linewidth=3)
-    plt.plot([logit_true]*(RT+1), '--', color='gray', linewidth=3)
+    plt.plot([logit_true]*(6000), '--', color='gray', linewidth=3)
     #plt.ylim([0,1])
-    plt.xlim([0,RT+1])
+    plt.xlim([0,6000])#RT+1])
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.legend(fontsize=14, loc=4)
     plt.ylabel('logit $q(a_1)$',fontsize=16)
     plt.xlabel('sample number',fontsize=16)
-    plt.savefig('logit_sampling_run_'+str(npi)+'_'+str(j)+'.svg')
-    plt.savefig('logit_sampling_run_'+str(npi)+'_'+str(j)+'.png')
+    plt.savefig(os.path.join(folder,'logit_sampling_run_npi'+str(npi)+'_f'+str(f)+'_'+str(j)+'.svg'))
+    plt.savefig(os.path.join(folder,'logit_sampling_run_npi'+str(npi)+'_f'+str(f)+'_'+str(j)+'.png'))
     plt.show()
 
     plt.figure()
@@ -75,7 +99,7 @@ def show_sampling_trajectory(samples, RT, npi, j=0):
 
 
 npi = 81
-l_val = 0.16
+l_val = 0.525
 p_val = 0.1
 
 prior = np.zeros(npi)
@@ -90,41 +114,45 @@ post /= post.sum()
 
 trials = 2
 
-for i in range(10):
+f = 0.5
 
-    RTs, all_samples = run_action_selection(post, prior, like, trials)
+for i in range(20):
 
-    ind = 0
-
-    samples = all_samples[ind]
-    RT = RTs[ind]
-
-    show_sampling_trajectory(samples, RT, npi, i)
-
-
-npi = 2
-l_val = 0.8
-p_val = 0.6
-
-prior = np.zeros(npi)
-prior[:] = (1-p_val) / (npi-1)
-prior[1] = p_val
-like = np.zeros(npi)
-like[0] = l_val
-like[1:] = (1-l_val) / (npi-1)
-
-post = prior*like
-post /= post.sum()
-
-trials = 2
-
-for i in range(10):
-
-    RTs, all_samples = run_action_selection(post, prior, like, trials, crit_factor=2.)
+    RTs, all_samples = run_action_selection(post, prior, like, trials, crit_factor=f)
 
     ind = 0
 
     samples = all_samples[ind]
     RT = RTs[ind]
 
-    show_sampling_trajectory(samples, RT, npi, i)
+    show_sampling_trajectory(samples, RT, npi, i, crit_factor=f)
+
+
+# npi = 2
+# l_val = 0.8
+# p_val = 0.6
+
+# prior = np.zeros(npi)
+# prior[:] = (1-p_val) / (npi-1)
+# prior[1] = p_val
+# like = np.zeros(npi)
+# like[0] = l_val
+# like[1:] = (1-l_val) / (npi-1)
+
+# post = prior*like
+# post /= post.sum()
+
+# trials = 2
+
+# f = 1.5
+
+# for i in range(20):
+
+#     RTs, all_samples = run_action_selection(post, prior, like, trials, crit_factor=f)
+
+#     ind = 0
+
+#     samples = all_samples[ind]
+#     RT = RTs[ind]
+
+#     show_sampling_trajectory(samples, RT, npi, i, crit_factor=f)
