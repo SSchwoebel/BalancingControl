f0f390edca441955292c5f503b752bd03fe9d039
SIHranova <31134739+SIHranova@users.noreply.github.com>
2024-11-01 18:59:59 +0100

    stable agent and well aranged plotting

--- TGT_simulation_and_analysis_utils.py
+++ TGT_simulation_and_analysis_utils.py
@@ -3,10 +3,14 @@
 import sys
 import os
 import numpy as np
+import copy 
+
 
 ###
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
 
 from matplotlib.animation import FuncAnimation
 plt.rcParams['animation.embed_limit'] = 400
@@ -92,7 +96,7 @@ def run_single_simulation(pars):
                                  T = pars["T"],
                                  number_of_actions = na)
     
-    # sanity check printing
+    ### print simulation values for the log
     vals = ['alpha_0', 'dec_temp', 'context_trans_prob', 'run', 'learn_pol', 'learn_rew', 'learn_context_obs', 'reward_count_bias',  'prior_rewards', 'all_rewards', 'hidden_state_mapping', 'nm', 'nh']
     matrix_vals = ['generative_model_context', 'dirichlet_context_obs_params', 'transition_matrix_context']
 
@@ -158,9 +162,7 @@ def create_data_frame(exp_name, current_dir, data_folder="raw_data"):
         context_cue =  pars["context_observation"].repeat(T)
         context_trans_prob =  np.array(pars["context_trans_prob"]).repeat(factor)
         utility = [list(pars["prior_rewards"])]*factor
-        print(fi)
-        print(perc.posterior_dirichlet_context_obs[0].round(3))
-        # print(perc.learn_context_obs)
+
         rewards = perc.rewards.flatten("C")
         state = env.state_mapping[np.arange(n_trials)[:,None],perc.observations].flatten("C")
         agent = np.ones(factor)*fi
@@ -290,6 +292,20 @@ def plot_context_entropy_t_alpha_rho(dataframe, simulation_params):
 
         plt.suptitle(fr"Mean context entropy as a function $t$, for each trial type and different $\rho$;" + title,y=1.1);
 
+def plot_individual_agents(dataframe, alpha_0=1, dec_temp=3, context_trans_prob=0.6, t=3):
+    
+    df = dataframe.copy()
+    context = df.groupby(["alpha_0","dec_temp","context_trans_prob","agent","file","trial_type","block","context_cue","t"])[["inferred_correct_context"]].mean().reset_index()
+
+    fig, ax = plt.subplots(1,2,figsize=(7,3))
+    
+    for cue in range(2):
+        df = context.query(f"alpha_0 == {alpha_0} & dec_temp=={dec_temp} & context_trans_prob=={context_trans_prob} & context_cue=={cue} & t=={t}")
+        plt.figure()
+        g = sns.lineplot(ax=ax[cue],data=df, x="block", y="inferred_correct_context",hue="file",palette="viridis")
+        g.set_xticks(np.arange(13));
+        g.set_ylim([0,1.2])
+        g.set_title(f"context_cue = {cue}")
 
 def plot_average_DKL(rho, dataframe, simulation_params):
     
@@ -321,3 +337,82 @@ def animate_histogram(data, interval=500):
     html = HTML(animation.to_jshtml())
     display(html)
     plt.close() # update
+    
+
+def animate_multiple_histograms(matrices, bins=10, interval=200):
+
+    N = matrices[0].shape[0]
+    n_matrices = len(matrices)
+    
+    # Create figure and axes for subplots
+    fig, axes = plt.subplots(1, n_matrices, figsize=(3.5*n_matrices, 5))
+    
+    def update(frame):
+        # Clear each axis for the new frame
+        for ax, matrix in zip(axes, matrices):
+            ax.clear()
+            # sns.histplot(matrix[frame].flatten(), bins=bins, kde=False, ax=ax, color="blue")
+            sns.heatmap(matrix[frame], annot=True, cmap="viridis", cbar=False, fmt='.2f',ax=ax)
+            ax.set_title(frame % 6 + 1)
+    # Create animation
+    anim = FuncAnimation(fig, update, frames=N, interval=interval, repeat=True)
+
+    html = HTML(anim.to_jshtml())
+    display(html)
+    plt.close() # u
+    
+    
+def plot_heatmap(data,share_x=True, share_y=True):
+    if not isinstance(data,list):
+        data = [data]
+    n_axes = len(data)
+    
+    fig, axes = plt.subplots(1,n_axes, figsize=(3*n_axes, 3),sharex=share_x, sharey= share_y)
+    plt.tight_layout()
+    if n_axes == 1:
+        axes = np.array([axes])
+
+    for ai in range(n_axes):
+        sns.heatmap(data[ai], annot=True, cmap="viridis", cbar=False, fmt='.2f',ax=axes[ai]);
+        
+    return fig,axes
+
+
+def plot_task_structure(experiment_config):
+    
+    exp_params = copy.deepcopy(experiment_config["experiment_data"])
+    exp_params.pop("planets")
+    df = pd.DataFrame(exp_params).reset_index()
+
+
+    fig, axes = plt.subplots(1,2,figsize=(11,3))
+    
+
+    for bi, block in enumerate([0,experiment_config["meta_data"]["training_blocks"]]):
+        
+        ax = axes[bi]
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(3))
+        sns.lineplot(ax=ax, data=df.query(f"block == {block}"), x="index", y=df["context"] % 2,color='k',marker="o",label="habit=0 or planing=1 context")
+        pivoted = df.query(f"block == {block}").pivot(index="index", columns="context_observation", values="optimal_policy")
+        pivoted.plot(ax=ax, marker="o",label="optimal_policy")# sns.lineplot(data=df.query("block == 0 "), x="index",y="context")
+        ax.set_title(f"block {block}")
+        ax.legend(bbox_to_anchor=(1.1, 0.5))
+        
+        if bi == 0:
+            ax.get_legend().remove()
+
+def plot_reward_probs(contingency_1, contingency_2):
+    fig,axes = plot_heatmap([contingency_1, contingency_2])
+    fig.suptitle(r"Reward Contingencies during Training and Degradation $p(r|s)$",y=1.05)
+
+    for ax in axes:
+        ax.set_xlabel("planets")
+        ax.set_ylabel("rewards")
+
+def plot_state_transition_matrix(stm): 
+    fig,axes = plot_heatmap([stm[:,:,0].T, stm[:,:,1].T])
+    fig.suptitle(r"$p(s_t|s_{t-1},a)$",y=1.05)
+
+    for ax in axes:
+        ax.set_xlabel(r"$s_{t-1}$")
+        ax.set_ylabel(r"$s_{t}$")
