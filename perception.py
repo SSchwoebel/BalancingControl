6e9b3df8ba9c49a9bf7a82d1693869c8fcaab7f6
SSchwoebel <sarah.schwoebel@gmail.com>
2026-01-19 11:29:11 +0100

    changed default value in context perception anfd added mask to tmaze_utils

--- perception.py
+++ perception.py
@@ -490,7 +490,7 @@ class Group2ContextPerception(object):
                  prior_rewards,
                  prior_context,
                  policies,
-                 alpha_0 = ar.tensor([1]),
+                 alpha_0 = ar.tensor([0]),
                  cached_reward_params = None,
                  dirichlet_rew_params = None,
                  dirichlet_context_obs_params = None,
@@ -946,11 +946,6 @@ class Group2ContextPerception(object):
         log_prior = ar.log(self.prior_policies[-1]+1e-10)
         # weighted_log_prior = self.hab_bias[None,...]*self.mask[tau][None,...]*log_prior
         prior = ar.exp(self.hab_bias[None,...]*self.mask[tau][None,...]*log_prior).to(device)
-
-        log_cached = ar.log(self.cached_policy_val[-1]+1e-10)
-        # weighted_log_cached = self.cached_weight[None,...] * log_cached
-        # log_post += weighted_log_cached
-        cached = ar.exp(self.cached_weight[None,...]*self.mask[tau][None,...] * log_cached)
 
         log_cached = ar.log(self.cached_policy_val[-1]+1e-10)
         # weighted_log_cached = self.cached_weight[None,...] * log_cached
